582e6f5d2fddb298afa79e4b2600711cd47ca4d3
markhuang1994 <markhuang1993@gmail.com>
2019-10-29 14:54:32 +0800

    [Add]Rm cache file script

---
+++ src/main/resources/remove_cache.sh
@@ -0,0 +1,3 @@
+find "${HOME}/.log_cache" -mtime +10 -type f -exec rm "{}" \;
+find "${HOME}/lvadmin/history" -mtime +10 -type f -exec rm -r "{}" \;
+find /tmp -mtime +10 -type d -regex '^.*_log_history$' -exec rm -r "{}" \;
